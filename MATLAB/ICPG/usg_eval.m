0ff97ad231017ee2b45669c3f2c3f12cc6a2a19e
mariohm1311 <mario.he.me3.0@gmail.com>
2017-04-06 17:55:31 +0200

    Restructure

--- MATLAB/ICPG/usg_eval.m
+++
@@ -1,8 +0,0 @@
-function [final_xvel,final_yvel,final_x,final_y] = usg_eval(t,xvel,yvel,y0,g,Ve,tau,theta)
-    body_rad = 6371000;
-    final_xvel = Ve.*log(tau./(tau-t)).*cos(theta) + xvel;
-    final_yvel = Ve.*log(tau./(tau-t)).*sin(theta) + ((xvel.*xvel)./(body_rad+y0)-g).*t + yvel;
-    final_x = -Ve.*((tau-t).*log(tau./(tau-t))-t).*cos(theta) + xvel.*t;
-    final_y = -Ve.*((tau-t).*log(tau./(tau-t))-t).*sin(theta) + 0.5.*((xvel.*xvel)./(body_rad+y0)-g).*t.*t + yvel.*t + y0;
-end
-
